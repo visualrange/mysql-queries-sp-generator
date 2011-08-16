488700d066b9b2296440a3df18b897a6deb40e35
visualrange <visualrange@gmail.com>
2011-08-16 09:01:11 +0000

    adding solution & files

---
+++ MySQL SP Query Generator/Main.Designer.cs
@@ -0,0 +1,421 @@
+﻿namespace MySQL_SP_Query_Generator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDB = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.clbTables = new System.Windows.Forms.CheckedListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rbInline = new System.Windows.Forms.RadioButton();
+            this.rbStoredProc = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnSelectAll = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.txtDB);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtUserId);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtServer);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(868, 53);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection Information";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(67, 23);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(100, 22);
+            this.txtServer.TabIndex = 1;
+            this.txtServer.Text = "localhost";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(213, 23);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(50, 22);
+            this.txtPort.TabIndex = 3;
+            this.txtPort.Text = "3306";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "User ID";
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Location = new System.Drawing.Point(341, 23);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(72, 22);
+            this.txtUserId.TabIndex = 5;
+            this.txtUserId.Text = "root";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(419, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(494, 23);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(73, 22);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(576, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "DB";
+            // 
+            // txtDB
+            // 
+            this.txtDB.Location = new System.Drawing.Point(609, 23);
+            this.txtDB.Name = "txtDB";
+            this.txtDB.Size = new System.Drawing.Size(150, 22);
+            this.txtDB.TabIndex = 9;
+            this.txtDB.Text = "bbk";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(892, 26);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(48, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(778, 21);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // clbTables
+            // 
+            this.clbTables.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clbTables.FormattingEnabled = true;
+            this.clbTables.Location = new System.Drawing.Point(3, 18);
+            this.clbTables.Name = "clbTables";
+            this.clbTables.Size = new System.Drawing.Size(161, 396);
+            this.clbTables.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.clbTables);
+            this.groupBox2.Location = new System.Drawing.Point(12, 131);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(167, 417);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select Tables";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnGenerate);
+            this.groupBox3.Controls.Add(this.checkBox4);
+            this.groupBox3.Controls.Add(this.checkBox3);
+            this.groupBox3.Controls.Add(this.checkBox2);
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Controls.Add(this.rbStoredProc);
+            this.groupBox3.Controls.Add(this.rbInline);
+            this.groupBox3.Location = new System.Drawing.Point(188, 99);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(692, 51);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Select Action";
+            // 
+            // rbInline
+            // 
+            this.rbInline.AutoSize = true;
+            this.rbInline.Location = new System.Drawing.Point(24, 18);
+            this.rbInline.Name = "rbInline";
+            this.rbInline.Size = new System.Drawing.Size(105, 21);
+            this.rbInline.TabIndex = 0;
+            this.rbInline.TabStop = true;
+            this.rbInline.Text = "Inline Query";
+            this.rbInline.UseVisualStyleBackColor = true;
+            // 
+            // rbStoredProc
+            // 
+            this.rbStoredProc.AutoSize = true;
+            this.rbStoredProc.Location = new System.Drawing.Point(135, 18);
+            this.rbStoredProc.Name = "rbStoredProc";
+            this.rbStoredProc.Size = new System.Drawing.Size(141, 21);
+            this.rbStoredProc.TabIndex = 1;
+            this.rbStoredProc.TabStop = true;
+            this.rbStoredProc.Text = "Stored Procedure";
+            this.rbStoredProc.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(293, 18);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(69, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Select";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(368, 18);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(65, 21);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Insert";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(433, 18);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(76, 21);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Update";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(515, 18);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(71, 21);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "Delete";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(592, 16);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(94, 23);
+            this.btnGenerate.TabIndex = 6;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtResult);
+            this.groupBox4.Location = new System.Drawing.Point(188, 158);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(692, 387);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.Color.White;
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResult.Location = new System.Drawing.Point(3, 18);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(686, 366);
+            this.txtResult.TabIndex = 0;
+            // 
+            // btnSelectAll
+            // 
+            this.btnSelectAll.Location = new System.Drawing.Point(15, 102);
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(105, 23);
+            this.btnSelectAll.TabIndex = 6;
+            this.btnSelectAll.Text = "Select All";
+            this.btnSelectAll.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Click += new System.EventHandler(this.btnSelectAll_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 560);
+            this.Controls.Add(this.btnSelectAll);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MySQL SP & Query Generator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.CheckedListBox clbTables;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton rbStoredProc;
+        private System.Windows.Forms.RadioButton rbInline;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnSelectAll;
+    }
+}
+
---
+++ MySQL SP Query Generator/Main.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace MySQL_SP_Query_Generator
+{
+    public partial class Main : Form
+    {
+        string server, port, userId, password, database;
+        MySqlConnection conn;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            conn = GetConnection();
+            
+            try
+            {
+                conn.Open();
+                this.BindTables();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+            finally
+            {
+                conn.Close();
+                conn = null;
+            }
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            server = txtServer.Text.Trim();
+            port = txtPort.Text.Trim();
+            userId = txtUserId.Text.Trim();
+            password = txtPassword.Text.Trim();
+            database = txtDB.Text.Trim();
+
+            string connString = string.Format("server={0}; port={1}; uid={2}; pwd={3}; database={4};",server,port,userId,password,database);
+            conn = new MySqlConnection(connString);
+            return conn;
+        }
+
+        private void BindTables()
+        {
+            string query = string.Format("show tables from {0}",database);
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+
+            try
+            {
+                adapter.Fill(dt);
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    clbTables.Items.Add(dt.Rows[i][0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+
+            
+                
+        }
+
+        private void btnSelectAll_Click(object sender, EventArgs e)
+        {
+            if (clbTables.Items.Count > 0)
+            {
+                if(btnSelectAll.Text.Equals("Select All"))
+                {
+                    for (int i = 0; i < clbTables.Items.Count; i++)
+                    {
+                        clbTables.SetItemChecked(i, true);
+                    }
+
+                    btnSelectAll.Text = "Un-Select All";
+                    return;
+                }
+
+                if (btnSelectAll.Text.Equals("Un-Select All"))
+                {
+                    for (int i = 0; i < clbTables.Items.Count; i++)
+                    {
+                        clbTables.SetItemChecked(i, false);
+                    }
+
+                    btnSelectAll.Text = "Select All";
+                    return;
+                }
+            }
+                 
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (rbInline.Checked == true)
+            {
+                this.GenerateInlineQuery();
+            }
+            else if (rbStoredProc.Checked == true)
+            {
+                this.GenerateStoredProcedure();
+            }
+        }
+
+        private void GenerateInlineQuery()
+        { }
+
+        private void GenerateStoredProcedure() 
+        {
+            conn = this.GetConnection();
+
+            for (int i = 0; i < clbTables.CheckedItems.Count; i++)
+            {
+                string tableName = clbTables.CheckedItems[i].ToString();
+                try
+                {
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                    }
+
+                    DataTable dtInfo = new DataTable();
+                    string query = string.Format("explain {0}",tableName);
+                    MySqlCommand cmd = new MySqlCommand(query,conn);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter();
+                    adapter.SelectCommand = cmd;
+                    adapter.Fill(dtInfo);
+
+                    this.GenerateSelectStoredProcedure(tableName, dtInfo);
+                    this.GenerateInsertStoredProcedure(tableName, dtInfo);
+                    this.GenerateUpdateStoredProcedure(tableName, dtInfo);
+                    this.GenerateDeleteStoredProcedure(tableName, dtInfo);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+            conn = null;
+        }
+
+        private void GenerateSelectStoredProcedure(string tableName, DataTable tableInfo)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("DELIMITER $$");
+            sb.AppendLine(string.Format("CREATE PROCEDURE `{0}`.`{1}_Get` ()",database,tableName));
+            sb.AppendLine("BEGIN");
+
+            for (int i = 0; i < tableInfo.Rows.Count; i++)
+            { 
+                
+            }
+
+            sb.AppendLine("END");
+        }
+
+        private void GenerateInsertStoredProcedure(string tableName, DataTable tableInfo)
+        { }
+
+        private void GenerateUpdateStoredProcedure(string tableName, DataTable tableInfo)
+        { }
+
+        private void GenerateDeleteStoredProcedure(string tableName, DataTable tableInfo)
+        { }
+    }
+}
